34039a7a2ca602808742334f0e885b98abaff41b
Leif Gensert <leif@propertybase.com>
2014-01-23 12:55:00 +0100

    inital commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Choose your eating place
+
+    git clone https://github.com/leifg/mahlzeit.rb.git
+    cd mahlzeit.rb
+    ./choose.rb
---
+++ choose.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+places = [
+  'Asia Today',
+  'Dragon Lounge',
+  'Döner',
+  'Hans im Glück',
+  'Sinans',
+  'Tryp',
+]
+
+puts "Today we are going to: #{places.sample.inspect}"
